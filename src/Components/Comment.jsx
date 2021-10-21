4da194cd368a65c65efc5ae44c511a8fadbf9472
Shubham Kumar <shubham1406kumar@gmail.com>
2021-10-21 20:27:00 +0530

    Comments

---
+++ src/Components/Comment.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+import styles from "./comments.module.css"
+
+export default function Comment({ replies }) {
+  const [show,setShow]=useState(false)
+    return (
+        <div className={styles.comments}>
+            <div className={styles.comentBox}>
+        <div className={`show === true ? ${styles.close} : ${styles.line}`} onClick={() => {
+                setShow(!show)
+          }}>
+          +</div>
+      
+        <div className={styles.box}>
+            <div className={styles.subH}>{replies.author}  { replies.points}Points {replies.timestamp}</div>
+            <div className={styles.head}>{replies.body}</div>
+            
+            <div className={styles.btn}>
+                <button>reply</button>
+                <button>forward</button>
+                <button>give award</button>
+                <button>share report</button>
+                <button>save</button>
+            </div>
+        </div>
+    
+        {show && (
+            <div>{replies.replies?replies.replies.map((data) => {
+                return <Comment replies={ data}/>
+            }):""}</div>
+        )}
+            </div>
+            </div>
+       
+    )
+}
